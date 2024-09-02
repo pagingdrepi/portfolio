37dbe13992ecf756c98067b2a4d2c44804c5266a
cairo <177596729+pagingdrepi@users.noreply.github.com>
2024-09-01 20:37:52 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     lanyard = json.loads(requests.get('https://api.lanyard.rest/v1/users/1005856471122198558').text)
+    commits = json.loads(requests.get('https://api.github.com/repos/pagingdrepi/portfolio/commits').text)
+    
     spotify = lanyard['data']['spotify']
     title=None
     artist=None
@@ -14,8 +16,10 @@ def home():
         title = spotify['song']
         artist = spotify['artist'].split(';')[0]
     status = lanyard['discord_status']
+
+    message = commits[0]['message']
     
-    return render_template('index.html', spotify=spotify, title=title, artist=artist)
+    return render_template('index.html', spotify=spotify, title=title, artist=artist, commit=message)
 
 @app.route('/about')
 def about():

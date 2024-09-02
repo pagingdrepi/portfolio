067ae5785d4ab38196875f72799f1e02db9a9b4b
pagingdrepi <borkbork1289+ii@gmail.com>
2024-09-01 20:33:00 -0500

    stuff its gonna crash wtv

--- api/index.py
+++ api/index.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+from requests import get
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    lanyard = json.loads(requests.get('https://api.lanyard.rest/v1/users/1005856471122198558').text)
+    spotify = lanyard['data']['spotify']
+    title=None
+    artist=None
+    if spotify:
+        title = spotify['song']
+        artist = spotify['artist'].split(';')[0]
+    status = lanyard['discord_status']
+    
+    return render_template('index.html', spotify=spotify, title=title, artist=artist)
 
 @app.route('/about')
 def about():
